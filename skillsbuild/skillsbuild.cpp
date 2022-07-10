4a121e35847a7149e6d6772c84c761260e85f81d
Dell <Dell@DESKTOP-2CTBUI5>
2022-07-10 15:44:07 +0300

    Add project files.

---
+++ skillsbuild/skillsbuild.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	int a = 10;
+	int b = 15;
+	int c = a + b + 1;
+	cout << c <<"\n";
+	int numbers[5] = { 1,2,3,4,5 };
+	for (int i = 0; i < 5; i++)
+		cout << numbers[i] << "\n";
+}
